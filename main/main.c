0400450d55dae2b919daf7f01e7349d3c7ffee15
Newyork <spidermanwin1@gmail.com>
2024-10-30 22:59:32 +0700

    commit

---
+++ main/main.c
@@ -0,0 +1,64 @@
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+
+#define LED_PIN 27
+#define PUSH_BUTTON_PIN 33
+
+TaskHandle_t myTaskHandle = NULL;
+QueueHandle_t queue;
+
+void Task(void *arg)
+{
+	// 1. เตรียมพื้นที่รับข้อมูลผ่าน queue
+	char rxBuffer;
+	// 2. วนลูปรอ
+	while (1)
+	{
+		// 3. ถ้ามีการส่งข้อมูลผ่าน queue handle ที่รอรับ ให้เอาข้อมูลไปแสดงผล
+		// queue consumer จะใช้ฟังก์ชัน xQueueReceive
+		if (xQueueReceive(queue, &(rxBuffer), (TickType_t) 5))
+		{
+			printf("Button pressed!\n");
+			vTaskDelay(1000 / portTICK_PERIOD_MS);
+		}
+	}
+}
+
+void IRAM_ATTR button_isr_handler(void *arg)
+{
+	// code จาก https://www.freertos.org/a00119.html
+	char cIn;
+	BaseType_t xHigherPriorityTaskWoken;
+	/* We have not woken a task at the start of the ISR. */
+	xHigherPriorityTaskWoken = pdFALSE;
+	cIn = '1';
+	xQueueSendFromISR(queue, &cIn, &xHigherPriorityTaskWoken);
+}
+
+void app_main(void)
+{
+	esp_rom_gpio_pad_select_gpio(PUSH_BUTTON_PIN);
+	esp_rom_gpio_pad_select_gpio(LED_PIN);
+
+	gpio_set_direction(PUSH_BUTTON_PIN, GPIO_MODE_INPUT);
+	gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+	gpio_set_intr_type(PUSH_BUTTON_PIN, GPIO_INTR_POSEDGE);
+
+	gpio_install_isr_service(0);
+
+	gpio_isr_handler_add(PUSH_BUTTON_PIN, button_isr_handler, NULL);
+
+	// สร้าง queue ที่บรรจุตัวแปร char จำนวน 1 ตัว
+	queue = xQueueCreate(1, sizeof(char));
+
+	// ทดสอบว่าสร้าง queue สำเร็จ?
+	if (queue == 0)
+	{
+		printf("Failed to create queue= %p\n", queue);
+	}
+
+	xTaskCreatePinnedToCore(Task, "My_Task", 4096, NULL, 10, &myTaskHandle, 1);
+}
